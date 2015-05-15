547d3fcf424f6892721a7db43feca8724bfd64e2
gmurph52 <bobsmith52bananas@yahoo.com>
2015-05-15 11:00:13 -0600

    testing with netbeans

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello GitHub World");
+        System.out.println("Hello GitHub");
         System.out.println("testing..."); 
     }
     
