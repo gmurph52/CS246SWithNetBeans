054566b8e038a093ca6d8b53a90ff3f5cdbedceb
gmurph52 <bobsmith52bananas@yahoo.com>
2015-05-15 11:14:52 -0600

    testing hello world with netbeans

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,12 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
-        System.out.println("testing..."); 
+        System.out.println("testing...");  
+        
+        
+        
+        
+        
     }
     
 }
